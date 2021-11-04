695192be998eecc7fb879a8f217f495dde669a01
Arco Dielhof <arcodielhof@hotmail.com>
2021-11-04 12:39:04 +0100

    Update to prevent 'AddOn Out Of Date' stuff.

--- UnLockables.lua
+++ UnLockables.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["UnLockables"] = 9
+AZP.VersionControl["UnLockables"] = 10
 if AZP.UnLockables == nil then AZP.UnLockables = {} end
 if AZP.UnLockables.Events == nil then AZP.UnLockables.Events = {} end
 
