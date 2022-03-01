ebf4ea47d15c27491644727a587e76ca66d712ce
Arco Dielhof <arcodielhof@hotmail.com>
2022-03-01 11:37:53 +0100

    Update for patch 9.2 to no longer show as out of date!

--- UnLockables.lua
+++ UnLockables.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["UnLockables"] = 10
+AZP.VersionControl["UnLockables"] = 11
 if AZP.UnLockables == nil then AZP.UnLockables = {} end
 if AZP.UnLockables.Events == nil then AZP.UnLockables.Events = {} end
 
